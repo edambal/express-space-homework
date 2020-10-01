09f5cb1d7a1bd851963f78bfb13ec4eae5874f80
edambal <bal818edam@gmail.com>
2020-09-30 23:09:45 -0700

    complete linkig and images added

--- controller/marsController.js
+++ controller/marsController.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const marsM = require('../models/marsMissions');
+const images =['https://www.sciencemag.org/sites/default/files/styles/article_main_image_-_1280w__no_aspect_/public/sn-curiosity.jpg?itok=2XMLUPhh',
+'https://solarsystem.nasa.gov/system/content_pages/main_images/1057_1057_rover2_768.jpg',
+'https://solarsystem.nasa.gov/system/content_pages/main_images/1068_rover2-1.jpg',
+'https://upload.wikimedia.org/wikipedia/commons/3/3a/Sojourner_on_Mars_PIA01122.jpg',
+'https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2002/11/rosetta_swings_by_mars/9157617-5-eng-GB/Rosetta_swings_by_Mars_pillars.jpg'];
+
+for(let i=0;i<marsM.length;i++){
+    marsM[i]['img'] = images[i];
+}
 
 router.get('/',(req,res)=>{
     res.send("This is what it is");
@@ -12,5 +21,11 @@ router.get('/missions',(req,res)=>{
         missions : marsM
     })
 });
+
+router.get('/missions/:id',(req,res)=>{
+    res.render('./missions/showmissions',{
+        mission : marsM[req.params.id]
+    })
+})
 
 module.exports = router;
